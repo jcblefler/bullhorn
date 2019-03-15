4c8500ca4d8eeda1babc7fb5868488611a11387a
GBTC408005UR <GBTC408005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-15 10:04:30 -0400

    Bullhorn

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import com.cloudinary.utils.ObjectUtils;
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+//import org.thymeleaf.util.ObjectUtils;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @RequestMapping("/")
+    public String listMessages(Model model){
+        model.addAttribute("messages", messageRepository.findAll());
+
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "messageform";
+    }
+
+    @PostMapping("/process")
+    public String porcessForm(@Valid Message message, BindingResult result, @RequestParam("file")MultipartFile file){
+        if (result.hasErrors()){
+            return "messageform";
+        }
+        if (file.isEmpty()){
+            return "messageform";
+        }
+        try {
+            message.setPosteddate(LocalDateTime.now());
+            Map uploadResult =cloudc.upload(file.getBytes(), ObjectUtils.asMap("resourcetype", "auto"));
+            message.setImage(uploadResult.get("url").toString());
+            messageRepository.save(message);
+        } catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "messageform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delMessage(@PathVariable("id") long id){
+        messageRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
